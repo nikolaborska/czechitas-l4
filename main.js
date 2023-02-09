e4110718eb7ade5e3f681838b7e91a327ede6344
Filip Procházka <prochazka@unodor.cz>
2023-02-09 18:54:59 +0100

    ukazka objekty

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 // tady je místo pro náš program
 
+//const address = 'Pod Kaštany 31, 123 11 Horní Dlouhonosy'
+/*
+const adresa = {
+  ulice: 'Pod Kaštany',
+  cislo: 31,
+  mesto: 'Horní Dlouhonosy',
+  psc: '123 11'
+}
+*/
 
+const address = {
+  streetName: 'Pod Kaštany',
+  number: 31,
+  city: 'Horní Dlouhonosy',
+  postalCode: '123 11',
+}
 
+// let vstupUzivatel = prompt("Zadej správné číslo domu:")
+// address.number = Number(vstupUzivatel)
+
+const user1 = {
+  fullName: 'Lubomír Větvička',
+  login: 'lubos',
+  address: {
+    streetName: 'Pod Kaštany',
+    number: 31,
+    city: 'Horní Dlouhonosy',
+    postalCode: '123 11'
+  },
+  cartItems: 1
+}
+
+// DESTRUKTUROVÁNÍ
+
+const product = {
+  name: 'Mlýnek na kávu',
+  price: 520,
+  currency: 'Kč'
+}
+
+const {name, price, currency} = product
 
